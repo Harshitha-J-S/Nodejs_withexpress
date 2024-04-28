6d5af926a2fa17f5c9ea08c2ac75bcaf51b9f82f
Harshitha-JS <harshithajs04@gmail.com>
2024-04-28 21:12:54 +0530

    Get movie by ID and updated 404 status code for nonexistent ID

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json())  //to attach the request with requestbody we need to use
 // app.post('/',(req,res) => {
 //     //Data is sent to be processed to a specific resource (through some API URL)
 // })
-
+//**********to list all the movies */
 app.get('/api/v1/movies',(req,res) => {
     res.status(200).json({
         status:"success",
@@ -23,6 +23,30 @@ app.get('/api/v1/movies',(req,res) => {
             movies:movies // JSon format,above movies variable is assigned
         }
     })
+})
+//*********to return the movie based on given api ID  */
+// app.get('/api/v1/movies/:id/:name/:x') // if 4/chan--then it gives error coz x is not defined
+// app.get('/api/v1/movies/:id/:name/:x?')  //for the above argument it doesn't give error,coz its optional
+app.get('/api/v1/movies/:id',(req,res) => {  //:represents route parameter
+    // console.log(req.params)  // params store req parameter of route
+    
+    const id = req.params.id * 1 // to convert string to int
+    
+    let movie = movies.find(el => el.id === id) //el wil iterate through movies array
+    
+    if(!movie){
+        return res.status(404).json({
+           status:"fail",
+           message:'movie with id' + id + 'is not found'
+        })
+    }
+    
+    res.status(200).json({
+        status:"success",
+        data: {
+            movie:movie
+        }
+})
 })
 
 app.post('/api/v1/movies',(req,res) =>{
