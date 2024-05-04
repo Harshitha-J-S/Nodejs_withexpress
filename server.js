5e1399b457d56029a03705cb9b3923fcce5c5b50
Harshitha-J-S <jsharshithagowda@gmail.com>
2024-05-04 12:25:52 +0530

    mongodb and mongoose

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const dotenv = require('dotenv')
 dotenv.config({path:'./config.env'})
-
+const mongoose = require('mongoose')  // after env only mongoose must be there
 // npm start will run it
 const app = require('./app');
 
 console.log(process.env)
+
+mongoose.connect(process.env.CONN_STR, {
+    useNewUrlParser : true
+}).then((conn) => {
+     console.log(conn)
+     console.log('DB connection Successfull')
+}).catch((error) => {
+    console.log('Some error has occured');
+});
+
 
 const port = process.env.PORT || 3000;
 
